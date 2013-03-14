c8a5d850ec9888c5285732a3b6fbf0bf5229ac2b
stask <stask@me.com>
2013-03-14 09:08:18 +0200

    no need for switching meta anymore

--- init.d/mac-switch-meta.el
+++ init.d/mac-switch-meta.el
@@ -9,15 +9,15 @@
 (global-set-key [(hyper z)] 'undo)
 
 ;; mac switch meta key
-(defun mac-switch-meta nil
-  "switch meta between Option and Command"
-  (interactive)
-  (if (eq mac-option-modifier nil)
-      (progn
-        (setq mac-option-modifier 'meta)
-        (setq mac-command-modifier 'hyper))
-    (progn
-      (setq mac-option-modifier nil)
-      (setq mac-command-modifier 'meta))))
+;(defun mac-switch-meta nil
+;  "switch meta between Option and Command"
+;  (interactive)
+;  (if (eq mac-option-modifier nil)
+;      (progn
+;        (setq mac-option-modifier 'meta)
+;        (setq mac-command-modifier 'hyper))
+;    (progn
+;      (setq mac-option-modifier nil)
+;      (setq mac-command-modifier 'meta))))
 
-(mac-switch-meta)
+;(mac-switch-meta)
