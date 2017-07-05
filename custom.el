9bb3b5733c69410c52039e4dbb0e2013ff19d89b
stask <stask@me.com>
2017-07-05 15:30:18 +0300

    sync

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dockerfile-mode markdown-mode web-mode clj-refactor cider-eval-sexp-fu cider clojure-mode yasnippet paredit magit projectile expand-region shell-switcher exec-path-from-shell page-break-lines counsel swiper which-key ace-window solarized-theme use-package))))
+    (yaml-mode graphql-mode spaceline powerline counsel-projectile kaolin-theme js2-mode dockerfile-mode markdown-mode web-mode clj-refactor cider-eval-sexp-fu cider clojure-mode yasnippet paredit magit projectile expand-region shell-switcher exec-path-from-shell page-break-lines counsel swiper which-key ace-window solarized-theme use-package)))
+ '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
